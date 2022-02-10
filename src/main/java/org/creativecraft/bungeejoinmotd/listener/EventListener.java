afc06f0bf1e1ac6eacff630545d8e848d2146d5f
Brandon Nifong <brandon@tendency.me>
2022-02-09 19:48:26 -0600

    enhance: use ServerConnectedEvent instead of PostLoginEvent

--- src/main/java/org/creativecraft/bungeejoinmotd/listener/EventListener.java
+++ src/main/java/org/creativecraft/bungeejoinmotd/listener/EventListener.java
@@ -1,5 +1,6 @@
 package org.creativecraft.bungeejoinmotd.listener;
 
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
